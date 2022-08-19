c4c8c9d2c9c31c59ec216692ed13b68be21ca73a
Douglas Ventura <douglasventura.pe@gmail.com>
2022-08-18 21:24:19 -0300

    Map problem is done!

---
+++ generics-set-map/map-problem/src/application/App.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class App {
+    
+    public static void main(String[] args) {
+        
+        String path = "./../../in.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            
+            Map<String, Integer> votes = new LinkedHashMap<>();
+            String line = br.readLine();
+
+            while (line != null) {
+                String[] data = line.split(",");
+                String name = (String) data[0];
+                Integer numVotes = Integer.parseInt(data[1]);
+                
+                if (votes.containsKey(name)) {
+                    numVotes += votes.get(name);
+                    votes.put(name, numVotes);
+                } else {
+                    votes.put(name, numVotes);
+                }
+
+                line = br.readLine();
+            }
+
+            for (String candidate : votes.keySet()) {
+                System.out.println(candidate + ": " + votes.get(candidate));
+            }
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
